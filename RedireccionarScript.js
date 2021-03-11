112a547f448d89213db6cbd58f744713a95f4189
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-11 12:10:43 +0100

    Create RedireccionarScript.js

---
+++ RedireccionarScript.js
@@ -0,0 +1,8 @@
+<script language="JavaScript" type="text/javascript">
+var pagina="https://github.com/yanlimeng"
+function redireccionar()
+{
+location.href=pagina
+}
+setTimeout ("redireccionar()", 20000);
+</script>
